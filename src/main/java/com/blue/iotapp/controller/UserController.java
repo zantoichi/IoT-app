d115aa79fbcbb1c506315a9e06d026f71c93fbad
thanos loukeris <myemailid@domain.com>
2018-11-26 19:03:40 +0200

    Added to controller get a single user

--- src/main/java/com/blue/iotapp/controller/UserController.java
+++ src/main/java/com/blue/iotapp/controller/UserController.java
@@ -3,7 +3,9 @@ package com.blue.iotapp.controller;
 import com.blue.iotapp.model.User;
 import com.blue.iotapp.repository.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.Collection;
 import java.util.stream.Collectors;
@@ -22,4 +24,10 @@ public class UserController {
         return StreamSupport.stream(userRepository.findAll().spliterator(), false)
                 .collect(Collectors.toList());
     }
-}
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable Long id){
+        return userRepository.findById(id).get();
+    }
+    }
+
+
