6ffae801396d20037b29f2c6637778b3a2cf2e9a
zantoichi <s.vallianos@acg.edu>
2018-11-28 17:38:36 +0200

    Added endpoints for removing/assigning devices to/from users. Starting implementation for user/device removal

--- src/main/java/com/blue/iotapp/controller/DeviceController.java
+++ src/main/java/com/blue/iotapp/controller/DeviceController.java
@@ -2,7 +2,9 @@ package com.blue.iotapp.controller;
 
 import com.blue.iotapp.model.Device;
 import com.blue.iotapp.repository.DeviceRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -17,5 +19,12 @@ public class DeviceController {
     @GetMapping("/devices")
     public List<Device> getDevices(){
         return deviceRepository.findAll();
+    }
+
+    @DeleteMapping("/devices/removeDevice/{deviceId}")
+    public List<Device> removeDevice(@PathVariable Long deviceId) {
+        deviceRepository.deleteById(deviceId);
+        List<Device> devices = deviceRepository.findAll();
+        return devices;
     }
 }
